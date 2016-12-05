9fa2b6d49b2dbc5161f5e00e9aba9cd8e34d13a4
rhamill1 <rhamill11@gmail.com>
2016-12-04 17:50:34 -0800

    adds project create form and methods

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,4 +4,20 @@ class ProjectsController < ApplicationController
     @projects = Project.all
   end
 
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.create(project_params)
+    @project.save
+    redirect_to root_path
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:project_name,:description,:sub_title,:primary_image,:index_image,:github,:url,:tech,:completion_date)
+  end
+
 end
