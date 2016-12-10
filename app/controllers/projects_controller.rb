e1ddb4a1f8e5c8cc1687ed60a37378fe198345e2
rhamill1 <rhamill11@gmail.com>
2016-12-09 20:45:38 -0800

    adds project show page and action

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,7 @@
 class ProjectsController < ApplicationController
 
+  before_action :get_project, only: [:show]
+
   def index
     @projects = Project.all
   end
@@ -14,10 +16,17 @@ class ProjectsController < ApplicationController
     redirect_to root_path
   end
 
+  def show
+  end
+
   private
 
   def project_params
     params.require(:project).permit(:project_name,:description,:sub_title,:primary_image,:index_image,:github,:url,:tech,:completion_date)
+  end
+
+  def get_project
+    @project = Project.find(params[:id])
   end
 
 end
