1a7eb34d147724ba9fca22533897f9f3e905431e
rhamill1 <rhamill11@gmail.com>
2016-12-09 18:29:38 -0800

    deploy try

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,6 @@ module Portfolio
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.action_dispatch.default_headers.clear
   end
 end
