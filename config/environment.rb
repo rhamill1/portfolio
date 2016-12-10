75f3b06431500f2f9ba1423e2ac7a68120a46ae2
rhamill1 <rhamill11@gmail.com>
2016-12-09 18:26:11 -0800

    fixes deploy

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,5 @@
 # Load the Rails application.
 require_relative 'application'
-require 'neo4j/railtie'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-config.action_dispatch.default_headers.clear
