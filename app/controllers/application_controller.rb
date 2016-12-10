c19270e1e68415ac3b38d8d95bc90247b2452411
rhamill1 <rhamill11@gmail.com>
2016-12-09 18:01:24 -0800

    deployment test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  config.action_dispatch.default_headers = {
+    'X-Frame-Options' => 'ALLOWALL'
+  }
 end
