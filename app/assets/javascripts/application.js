f932c4996c83e4c39ad8a5268bd81645e14d6b5a
rhamill1 <rhamill11@gmail.com>
2016-12-15 20:11:27 -0800

    adds scroll on click to chevron

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,12 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+window.onload = function() {
+
+  $( "#chevron-scroll" ).click(function() {
+    $("html, body").animate({ scrollTop: 650 }, 1000);
+  });
+
+}
